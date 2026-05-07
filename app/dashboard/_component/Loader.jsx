dd6b1b06aff7320c29b52ff094b31b771c2e498a
Anish Singh <anish1279singh@gmail.com>
2026-05-07 19:28:53 +0530

    Deleted Duplicate files

--- app/dashboard/_component/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center mt-40">
-      <div className="w-24 h-24 border-8 text-black text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue rounded-full"></div>
-    </div>
-  );
-};
-
-export default Loader;
